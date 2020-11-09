a99bfbbaf01e5f5892118303a45c51b7ce3de30e
hexome <northloi@163.com>
2020-11-09 16:31:42 +0800

    组件

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="container">
-    add pagess
+  <div class="form-group">
+    <label for="exampleInputEmail1">添加待办</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model='inputValue'>
+    <small id="emailHelp" class="form-text text-muted">回车即可加入</small>
   </div>
+  <ul class="list-group">
+    <li class="list-group-item">
+       <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script lang='ts'>
 
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, ref } from 'vue'
 export default defineComponent({
   setup () {
-    return {}
+    const inputValue = ref('')
+    return reactive({
+      inputValue
+    })
   }
 })
 </script>
